ef25efbdae9d3c785be8bdb6f0d9c02e473ce82e
codefuldom <dominiquehallan@gmail.com>
2019-04-19 23:03:58 -0500

    :tada: import statement understanding

--- graphql-basics/src/myModule.js
+++ graphql-basics/src/myModule.js
@@ -1,5 +1,8 @@
 const message = 'Some module from myModule.js'
 const name = 'Dominique Hallan'
 const location = 'Independence, KS'
+const getGreeting = (name) => {
+  return `Welcome to the course, ${name}!`
+}
 
-export { message, name, location as default }
+export { message, getGreeting, name, location as default }
