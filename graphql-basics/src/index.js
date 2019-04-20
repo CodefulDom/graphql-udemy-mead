d303699f2ed94c2951e629879875db0da9ca4904
codefuldom <dominiquehallan@gmail.com>
2019-04-19 23:03:58 -0500

    :tada: import statement understanding

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,5 +1,6 @@
-import currentLocation, { message, name } from './myModule'
+import currentLocation, { message, name, getGreeting } from './myModule'
 
 console.log(message)
 console.log(name)
 console.log(currentLocation)
+console.log(getGreeting('Doom!'))
