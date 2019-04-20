d460ee13daf16f47f50fc28f2f4ae3ee4528e38e
codefuldom <dominiquehallan@gmail.com>
2019-04-19 23:23:32 -0500

    :tada: remove junk

--- graphql-basics/src/index.js
+++
@@ -1,8 +0,0 @@
-import currentLocation, { message, name, getGreeting } from './myModule'
-import { add, subtract } from './math'
-console.log(message)
-console.log(name)
-console.log(currentLocation)
-console.log(getGreeting('Doom!'))
-console.log(add(1, 7))
-console.log(subtract(10, 7))
