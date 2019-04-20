fdcb4f356a5e1bfab1ca72c0e0a9f3b334bbac8a
codefuldom <dominiquehallan@gmail.com>
2019-04-19 23:45:13 -0500

    :tada: added addtional typeDefs and resolvers

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -4,6 +4,9 @@ import { GraphQLServer } from 'graphql-yoga'
 const typeDefs = `
 	type Query {
 		hello: String!
+		name: String!
+		location: String!
+		bio: String
 	}
 `
 
@@ -12,6 +15,24 @@ const resolvers = {
   Query: {
     hello() {
       return 'This is the hello query!'
+    },
+    name() {
+      return `Dom!`
+    },
+    location() {
+      return `St. Louis, Mo`
+    },
+    bio() {
+      return `My name is Dom and I live in STL.`
     }
   }
 }
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+})
+
+server.start(() => {
+  console.log('server is running')
+})
