72a929895858a0d5b44ae4026814c8c5b8abead3
codefuldom <dominiquehallan@gmail.com>
2019-05-27 22:47:43 -0500

    :fireworks: completed the add method in the module challenge

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -3,7 +3,8 @@ import { GraphQLServer } from 'graphql-yoga'
 // Type Definitions = Schema
 const typeDefs = `
 	type Query {
-		greeting(name: String): String!
+		greeting(name: String, position: String): String!
+		add(a: Float!, b: Float!): Float!
 		id: ID!
 		name: String!
 		age: Int!
@@ -30,9 +31,13 @@ const typeDefs = `
 // Resolvers
 const resolvers = {
   Query: {
+    add(parent, args, ctx, info) {
+      const sum = `${args.a + args.b}`
+      return sum
+    },
     greeting(parent, args, ctx, info) {
       if (args.name) {
-        return `Hello, ${args.name}!`
+        return `Hello, ${args.name}! You are my favorite ${args.position}!!`
       } else {
         return `Hello user!`
       }
