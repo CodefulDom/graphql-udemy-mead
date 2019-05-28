e319759b2bcdeb1c186381fa14c266ed74a740eb
codefuldom <dominiquehallan@gmail.com>
2019-05-27 20:42:13 -0500

    :fireworks: working on module challenge

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -8,6 +8,20 @@ const typeDefs = `
 		age: Int!
 		isEmployed: Boolean!
 		gpa: Float
+		isMarried: String!
+	}
+
+	type User {
+		id: ID!
+		name: String!
+		email: String!
+	}
+
+	type Post {
+		id: ID!,
+		title: String!
+		body!: String!,
+		published: Int!
 	}
 `
 
@@ -28,6 +42,17 @@ const resolvers = {
     },
     gpa() {
       return 4.0
+    },
+    isMarried() {
+      return true
+    }
+  },
+  post() {
+    return {
+      id: `isbn 374982374893274893245783462873647823648723`,
+      title: `Harry Potter does Harry Potter Thangs`,
+      author: `J.K. Rowling`,
+      body: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus commodi nulla dolores in similique, quis facere sit libero non perferendis illo tempora reprehenderit dolore voluptatem, quas voluptates aperiam omnis sint!`
     }
   }
 }
@@ -38,5 +63,5 @@ const server = new GraphQLServer({
 })
 
 server.start(() => {
-  console.log('server is running')
+  console.log(`server is running at http://localhost:4000`)
 })
